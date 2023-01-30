0bb87266f07cf96e3685cc2188501d7aa96dfa63
gnomboltun <leevaa0@inbox.ru>
2023-01-30 20:08:34 +0300

    Login view

---
+++ sneakers-admin/src/layouts/login/Login.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-app>
+        <v-main>
+            <v-container class="w-100 h-screen">
+                <v-row class="justify-center align-center h-100">
+                    <v-col xl="4">
+                        <router-view></router-view>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
